2de9425f7476046e52e0840b8bce82f5eec2494a
Monster <Monster@MONSTER>
2023-07-31 13:57:42 +0300

    Add project files.

---
+++ Casgem_Ajax/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Casgem_Ajax.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MONSTER\\SQLEXPRESS;initial catalog=CasgemAjaxDb; integrated security = true;");
+        }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
